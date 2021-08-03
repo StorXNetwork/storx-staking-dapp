275ccb97ee9a39b9666b0ebe46ca16f1e73141dc
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2021-08-03 21:04:10 +0530

    css changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,9 +37,17 @@ const ToggleLight = (
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-moon"
+      className="feather feather-sun"
     >
-      <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+      <circle cx="12" cy="12" r="5"></circle>
+      <line x1="12" y1="1" x2="12" y2="3"></line>
+      <line x1="12" y1="21" x2="12" y2="23"></line>
+      <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+      <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+      <line x1="1" y1="12" x2="3" y2="12"></line>
+      <line x1="21" y1="12" x2="23" y2="12"></line>
+      <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+      <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
     </svg>
   </span>
 );
@@ -56,17 +64,9 @@ const ToggleDark = (
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="feather feather-sun"
+      className="feather feather-moon"
     >
-      <circle cx="12" cy="12" r="5"></circle>
-      <line x1="12" y1="1" x2="12" y2="3"></line>
-      <line x1="12" y1="21" x2="12" y2="23"></line>
-      <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-      <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-      <line x1="1" y1="12" x2="3" y2="12"></line>
-      <line x1="21" y1="12" x2="23" y2="12"></line>
-      <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-      <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+      <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
     </svg>
   </span>
 );
@@ -241,7 +241,7 @@ class Header extends React.Component {
             </div>
 
             <div className="tMenu ml-1">
-              <div className="custom-dropdown-toggle" title="Menu">
+              <div className="custom-dropdown-toggle u-pointer" title="Menu">
                 <FontAwesomeIcon icon={faEllipsisH} />
               </div>
               <ul className="dropdown">
