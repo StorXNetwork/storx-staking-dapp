eb2917ec0f90bdaf7060ec2f18919b1d6b5555f9
Debchakra Majumder <dmajumder4292@gmail.com>
2023-08-31 23:41:04 +0530

    fix claim button issue and remove note

--- src/components/Staking/StakingForm.js
+++ src/components/Staking/StakingForm.js
@@ -157,20 +157,15 @@ class StakingForm extends React.Component {
               <span className="">{FormatNumber(fromWei(stake.balance))}</span>
             </li>
           </ul>
-          <div className="text-center mt-3 font-weight-bold font-italic">
-            Note: Node Operators are advised not to click claim options until a 7-day cooling period after 
-            un-staking is completed, as clicking before this could result in transaction rejections 
-            or refund delays.
-          </div>
           <div className="text-center mt-3">
-            <div
+            <button
               onClick={this.withdrawStake}
               disabled={parseFloat(stake.canWithdrawStakeIn) !== 0}
               href="claim-stake.html"
               className="btn btn-rounded btn-info"
             >
               Claim Stake
-            </div>
+            </button>
           </div>
         </div>
       );
