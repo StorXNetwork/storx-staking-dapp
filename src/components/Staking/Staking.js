a4a335a0fdc938069e5cfa92aaf073d8f465fded
Satish pethani <satyapethani@gmail.com>
2024-06-19 17:00:46 +0530

    Managing isStacker before calling nextDripAt

--- src/components/Staking/Staking.js
+++ src/components/Staking/Staking.js
@@ -5,11 +5,7 @@ import { fromWei, toWei } from "xdc3-utils";
 
 import { SubmitContractTxGeneral } from "../../wallets";
 import { LOADER_BOX } from "../common/common";
-import {
-  AxiosInstance,
-  CONTRACT_ADDRESS,
-  RemoveExpo,
-} from "../../helpers/constant";
+import { AxiosInstance, CONTRACT_ADDRESS, RemoveExpo } from "../../helpers/constant";
 import { InfoHeader, InfoCard } from "./Presentational";
 import StakingForm from "./StakingForm";
 
@@ -35,152 +31,80 @@ class Staking extends React.Component {
   }
 
   componentDidUpdate(prevProps) {
-    if (prevProps.wallet.address !== this.props.wallet.address)
-      this.getStakeDetail();
+    if (prevProps.wallet.address !== this.props.wallet.address) this.getStakeDetail();
   }
 
   claimEarned() {
     if (!this.props.wallet.connected) return;
-    SubmitContractTxGeneral(
-      "claimEarned",
-      { type: "staking" },
-      "nonpayable",
-      this.props.wallet.address
-    )
-      .then(console.log)
-      .catch(console.log);
+    SubmitContractTxGeneral("claimEarned", { type: "staking" }, "nonpayable", this.props.wallet.address).then(console.log).catch(console.log);
   }
 
-  getStakeDetail() {
+  async getStakeDetail() {
     if (!this.props.wallet.connected) return LOADER_BOX;
 
-    Promise.all([
-      SubmitContractTxGeneral(
-        "stakes",
-        { type: "staking" },
-        "view",
-        this.props.wallet.address
-      ),
-      SubmitContractTxGeneral(
-        "earned",
-        { type: "staking" },
-        "view",
-        this.props.wallet.address
-      ),
-      SubmitContractTxGeneral(
-        "nextDripAt",
-        { type: "staking" },
-        "view",
-        this.props.wallet.address
-      ),
-      SubmitContractTxGeneral(
-        "allowance",
-        { type: "storx" },
-        "view",
-        this.props.wallet.address,
-        CONTRACT_ADDRESS.staking
-      ),
-      AxiosInstance.get("/get-contract-data"),
-      SubmitContractTxGeneral(
-        "isStaker",
-        { type: "reputation" },
-        "view",
-        this.props.wallet.address
-      ),
-      SubmitContractTxGeneral(
-        "reputations",
-        { type: "reputation" },
-        "view",
-        this.props.wallet.address
-      ),
-    ])
-      .then(
-        ([
-          x,
+    const isStaker = await SubmitContractTxGeneral("isStaker", { type: "reputation" }, "view", this.props.wallet.address);
+
+    const stackingDetails = await SubmitContractTxGeneral("stakes", { type: "staking" }, "view", this.props.wallet.address);
+    const earned = await SubmitContractTxGeneral("earned", { type: "staking" }, "view", this.props.wallet.address);
+    const stakingData = await AxiosInstance.get("/get-contract-data");
+    const nextDripAt = isStaker
+      ? await SubmitContractTxGeneral("nextDripAt", { type: "staking" }, "view", this.props.wallet.address)
+      : Math.floor(new Date().getTime() / 1000).toString();
+    const approvedAmount = await SubmitContractTxGeneral("allowance", { type: "storx" }, "view", this.props.wallet.address, CONTRACT_ADDRESS.staking);
+    const reputation = await SubmitContractTxGeneral("reputations", { type: "reputation" }, "view", this.props.wallet.address);
+
+    const { exists, balance, lastRedeemedAt, unstakedTime, staked, totalRedeemed, stakerHolder, stakedAmount, stakedTime, unstaked, ...rst } =
+      stackingDetails;
+
+    console.log("nextDripAt", nextDripAt);
+
+    this.setState(
+      {
+        loaded: true,
+        stake: {
+          exists,
+          balance,
+          lastRedeemedAt,
+          unstakedTime,
+          staked,
+          totalRedeemed,
+          stakerHolder,
+          stakedAmount,
+          stakedTime,
           earned,
           nextDripAt,
-          approvedAmount,
-          stakingData,
-          isStaker,
-          reputation,
-        ]) => {
-          const {
-            exists,
-            balance,
-            lastRedeemedAt,
-            unstakedTime,
-            staked,
-            totalRedeemed,
-            stakerHolder,
-            stakedAmount,
-            stakedTime,
-            unstaked,
-            ...rst
-          } = x;
-
-          this.setState(
-            {
-              loaded: true,
+          unstaked,
+          ...rst,
+        },
+        approvedAmount: fromWei(RemoveExpo(approvedAmount)),
+        stakingData: stakingData.data.data,
+        isStaker,
+        reputation,
+      },
+      () => {
+        if (unstaked) {
+          SubmitContractTxGeneral("canWithdrawStakeIn", { type: "staking" }, "view", this.props.wallet.address).then((x) => {
+            // console.log("canWithdrawStakeIn", x);
+            this.setState({
               stake: {
-                exists,
-                balance,
-                lastRedeemedAt,
-                unstakedTime,
-                staked,
-                totalRedeemed,
-                stakerHolder,
-                stakedAmount,
-                stakedTime,
-                earned,
-                nextDripAt,
-                unstaked,
-                ...rst,
+                ...this.state.stake,
+                canWithdrawStakeIn: x,
               },
-              approvedAmount: fromWei(RemoveExpo(approvedAmount)),
-              stakingData: stakingData.data.data,
-              isStaker,
-              reputation,
-            },
-            () => {
-              if (unstaked) {
-                SubmitContractTxGeneral(
-                  "canWithdrawStakeIn",
-                  { type: "staking" },
-                  "view",
-                  this.props.wallet.address
-                ).then((x) => {
-                  // console.log("canWithdrawStakeIn", x);
-                  this.setState({
-                    stake: {
-                      ...this.state.stake,
-                      canWithdrawStakeIn: x,
-                    },
-                  });
-                });
-              }
-            }
-          );
+            });
+          });
         }
-      )
-      .catch();
+      }
+    );
   }
 
   render() {
-    const reputation = this.state.isStaker
-      ? this.state.reputation
-      : "NOT A STAKER";
+    const reputation = this.state.isStaker ? this.state.reputation : "NOT A STAKER";
 
-    let stakeValidClass =
-      this.state.isStaker &&
-      this.state.reputation >= this.state.stakingData.reputationThreshold
-        ? "text-success"
-        : "text-danger";
+    let stakeValidClass = this.state.isStaker && this.state.reputation >= this.state.stakingData.reputationThreshold ? "text-success" : "text-danger";
 
     const stakeValid = this.state.stakingData
       ? this.state.isStaker
-        ? (this.state.reputation >= this.state.stakingData.reputationThreshold)
-            .toString()
-            .toUpperCase()
+        ? (this.state.reputation >= this.state.stakingData.reputationThreshold).toString().toUpperCase()
         : "NOT A STAKER"
       : LOADER_BOX;
 
@@ -191,13 +115,8 @@ class Staking extends React.Component {
             <div className="row">
               <div className="col-sm-12">
                 <div className="announcement">
-                  <b>Note</b>: If you're facing issues with your reputation &
-                  redeeming rewards please refer the thread{" "}
-                  <a
-                    href="https://twitter.com/StorXNetwork/status/1450885741962096641"
-                    target="_blank"
-                    className="blue"
-                  >
+                  <b>Note</b>: If you're facing issues with your reputation & redeeming rewards please refer the thread{" "}
+                  <a href="https://twitter.com/StorXNetwork/status/1450885741962096641" target="_blank" className="blue" rel="noreferrer">
                     here
                   </a>
                 </div>
@@ -213,19 +132,10 @@ class Staking extends React.Component {
             />
             <div className="row mb-3">
               <div className="col-sm-12 col-md-6 col-lg-6 mb-3">
-                <InfoCard
-                  data={this.state}
-                  getStakeDetail={this.getStakeDetail}
-                  claimRewards={this.claimEarned}
-                  stakeValid={stakeValid}
-                />
+                <InfoCard data={this.state} getStakeDetail={this.getStakeDetail} claimRewards={this.claimEarned} stakeValid={stakeValid} />
               </div>
               <div className="col-sm-12 col-md-6 col-lg-6 mb-3">
-                <StakingForm
-                  stake={this.state.stake}
-                  getStakeDetail={this.getStakeDetail}
-                  approvedAmount={this.state.approvedAmount}
-                />
+                <StakingForm stake={this.state.stake} getStakeDetail={this.getStakeDetail} approvedAmount={this.state.approvedAmount} />
               </div>
             </div>
           </div>
